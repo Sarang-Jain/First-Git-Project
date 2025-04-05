c62b8d0e993b8c0474f7e008cec46cf78f75abd3
First-Git_Project <sjdatascientiest@gmail.com>
2025-04-06 01:19:53 +0530

    Added Hotel Booking Page

--- Data-Science/Hotel.py
+++ Data-Science/Hotel.py
@@ -2,27 +2,40 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+
 st.title("Welcome to Mayfair")
 st.write("Please start your hotel booking")
 
-# Input for user's name
-name = st.text_input("Enter Your Name")
+name=st.text_input("Enter Your Name")
 if name:
     st.write(f"Hello, {name}")
 
-# Room options
-room = ["Normal", "Delux", "Executive"]
-Selectedroom = st.selectbox("Select your preferred room", room, index=None)
-st.write(f"Your Preference: {Selectedroom}")
+room=["Normal","Delux","Executive"]
+Selectedroom=st.selectbox("Select your preferred room", room, index=None)
+st.write(f"Your Preference : {Selectedroom}")
 
-# Function to display room choice and cost
 def roomchoice(Selectedroom):
-    if Selectedroom == "Normal":
+    if Selectedroom=="Normal":
         st.write("Normal room selected. Cost approx 5000")
-    elif Selectedroom == "Delux":
+    elif Selectedroom=="Delux":
         st.write("Delux room selected. Cost approx 6000")
-    elif Selectedroom == "Executive":
-        st.write("Executive room selected. Cost approx 8000")
+    elif Selectedroom=="Executive":
+        st.write("Delux room selected. Cost approx 7000")
 
-# Call the function with the selected room
 roomchoice(Selectedroom)
+
+from datetime import datetime, tzinfo
+
+startdate= st.slider("Start date",value=datetime(2025,5,1), format="MM/DD/YY")
+st.write(f"Your Start Date:{startdate}")
+
+enddate= st.slider("End date",value=datetime(2025,5,1,9,30), format="MM/DD/YY-hh:mm:ss")
+st.write(f"Your End Date:{enddate}")
+
+Instruction=st.text_input("Note: Any instruction before checkin")
+if Instruction:
+    st.write(f"Your Instruction is noted")
+
+submission=st.button("Submit")
+if submission:
+    st.write("Congratulations, your booking is confirm")
